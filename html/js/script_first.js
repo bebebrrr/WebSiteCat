e6a613fe8d8bc2142d0eb119e01553d92375366a
bebebrrr <kubanova04@gmail.com>
2024-05-22 23:03:14 +0300

    scripts

---
+++ html/js/script_first.js
@@ -0,0 +1,10 @@
+function changeColor(){
+    document.getElementById('b1').addEventListener('click', function() {
+        // Показ сообщения
+        alert('Ваша заявка принята! Скоро с вами свяжется оператор');
+    });
+}
+
+
+
+    
