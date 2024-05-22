fbd5fc2c6a441e1c02eeacdca507409d88d23071
bebebrrr <kubanova04@gmail.com>
2024-05-22 23:27:42 +0300

    finally

---
+++ html/js/script_fifth.js
@@ -0,0 +1,8 @@
+function send(){
+    
+    document.getElementById('b1').addEventListener('click', function() {
+        // Показ сообщения
+        alert('Ваша заявка принята! Скоро с вами свяжется оператор');
+    });
+    
+}
