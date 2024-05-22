3790d2169b571de0fc8fb876ea55c7cc9467b695
bebebrrr <kubanova04@gmail.com>
2024-05-22 23:46:30 +0300

    labs

--- html/js/script_first.js
+++ html/js/script_first.js
@@ -5,6 +5,37 @@ function changeColor(){
     });
 }
 
+let currentImageIndex = 0;
+        const images = [
+        "content/gallary3.png",
+        "content/gallary2.png",
+        "content/gallary1.png",
+        "content/gallary4.jpg",
+        "content/gallary5.jpg",
+        "content/gallary6.jpg"
+        ];
 
+        document.addEventListener("DOMContentLoaded", () => {
+    // Initial update when the DOM content is loaded
+        document.getElementById("img1").src = images[0];
+        document.getElementById("img2").src = images[1];
+        document.getElementById("img3").src = images[2];
+});
+
+function photogalleryPrev() {
+    currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
+    updateGallery();
+}
+
+function photogalleryNext() {
+    currentImageIndex = (currentImageIndex + 1) % images.length;
+    updateGallery();
+}
+
+function updateGallery() {
+    document.getElementById("img1").src = images[currentImageIndex];
+    document.getElementById("img2").src = images[(currentImageIndex + 1) % images.length];
+    document.getElementById("img3").src = images[(currentImageIndex + 2) % images.length];
+}
 
     
